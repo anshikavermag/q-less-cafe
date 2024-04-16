c3d7c693fee40774e345fdbec20062a716e89222
Ayush Nagar <ayushnagar15tfe@gmail.com>
2024-04-17 04:27:10 +0530

    Enabled CORS

--- client/src/customer/Customer.jsx
+++ client/src/customer/Customer.jsx
@@ -1,4 +1,22 @@
+import { useEffect } from "react";
+
 function Customer() {
+    useEffect(() => {
+        console.log("CUSTOMER component mounted");
+        async function fetchData() {
+            try {
+                const response = await fetch(
+                    `${import.meta.env.VITE_API_URL}/orders`
+                );
+                const data = await response.json();
+                console.log(data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchData();
+    }, []);
+
     return <div>CUSTOMER</div>;
 }
 
--- server/app.js
+++ server/app.js
@@ -1,14 +1,38 @@
 import express from 'express';
+import cors from 'cors';
 import morgan from 'morgan';
 import outletRouter from './routes/outletRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
 
 const app = express();
 
+// Defining allowed origins
+let allowedOrigins = [
+    'https://www.qless.tech',
+    'http://localhost',
+    'http://localhost:5173',
+    'http://127.0.0.1',
+    'http://127.0.0.1:5173',
+];
+
+// Defining CORS options
+const corsOptions = {
+    origin: function (origin, callback) {
+        console.log('origin:', origin);
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+// Using morgan for logging HTTP requests
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 app.use(express.json());
-
 app.use('/api/v1/outlets', outletRouter);
 app.use('/api/v1/orders', orderRouter);
 
